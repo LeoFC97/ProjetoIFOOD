ca1244568e93f8ff07b9952c77d7c03c4814d386
Leonardo <leonardo.fc2@outlook.com>
2018-12-07 00:01:18 -0200

    ionic cordova plugin add cordova-plugin-network-information --save

--- ProjetoIFOOD.ionic/src/app/app.module.ts
+++ ProjetoIFOOD.ionic/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { SpinnerProvider } from '../providers/spinner/spinner';
 import { AlertProvider } from '../providers/alert/alert';
+import { HttpProvider } from '../providers/http/http';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { AlertProvider } from '../providers/alert/alert';
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     SpinnerProvider,
-    AlertProvider
+    AlertProvider,
+    HttpProvider
   ]
 })
 export class AppModule {}
---
+++ ProjetoIFOOD.ionic/src/providers/http/http.ts
@@ -0,0 +1,19 @@
+import { AlertProvider } from './../alert/alert';
+import { SpinnerProvider } from './../spinner/spinner';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the HttpProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class HttpProvider {
+
+  constructor(private http: HttpClient, private spinnerSrv: SpinnerProvider, private alertSrv: AlertProvider) {
+    console.log('Hello HttpProvider Provider');
+  }
+
+}
