3583660d1bcf95a06f292792b3b19bd63f2bffef
Leonardo <leonardo.fc2@outlook.com>
2018-12-10 23:58:58 -0200

    pagina produtos do mocelin

---
+++ ProjetoIFOOD.ionic/src/pages/mocelin-produtos/mocelin-produtos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MocelinProdutosPage } from './mocelin-produtos';
+
+@NgModule({
+  declarations: [
+    MocelinProdutosPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MocelinProdutosPage),
+  ],
+})
+export class MocelinProdutosPageModule {}
