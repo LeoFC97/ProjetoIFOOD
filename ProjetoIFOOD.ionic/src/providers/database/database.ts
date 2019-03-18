91686b5f52a585da0eb035de80cfde4d418d86de
Leonardo <leonardo.fc2@outlook.com>
2019-03-18 01:07:08 -0300

    codigo para criar as tabelas
    codigo para criar as tabelas

--- ProjetoIFOOD.ionic/src/providers/database/database.ts
+++ ProjetoIFOOD.ionic/src/providers/database/database.ts
@@ -1,17 +1,43 @@
+import { SQLite,SQLiteObject } from '@ionic-native/sqlite/ngx';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the DatabaseProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class DatabaseProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello DatabaseProvider Provider');
+  constructor(private sqlite: SQLite)
+  {
+
   }
+  public getDB()
+  {
+    return this.sqlite.create({
+      name: 'products.db',
+      location: 'default'
+    });
+
+  }
+  public createDatabase()
+  {
+    return this.getDB()
+    .then((db: SQLiteObject) =>
+    {
+      this.createTables(db);
+
+    })
+    .catch(e => console.error(e));
+  }
+
+  private createTables(db:SQLiteObject)
+  {
+    db.sqlBatch([
+      ['create table if not exists cliente (id integer primary key autoincrement not null, nome text, telefone text, email text, senha text, estado text, cidade text, bairro text, rua text, numero text, complemento text)']
+    ])
+    .then(()=> console.log('Tabelas criadas com sucesso'))
+    .catch(e =>console.error('Erro ao criar as tabelas', e));
+
+  }
+  
+
 
 }
