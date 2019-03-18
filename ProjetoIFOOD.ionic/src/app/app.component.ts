d70a757998459608b2eadc2d26477ce7ef23853e
Leonardo <leonardo.fc2@outlook.com>
2019-03-18 01:16:30 -0300

    corrigido erro da  aplicação gerar as tabelas antes do cordova carregar

--- ProjetoIFOOD.ionic/src/app/app.component.ts
+++ ProjetoIFOOD.ionic/src/app/app.component.ts
@@ -1,22 +1,37 @@
+import { DatabaseProvider } from './../providers/database/database';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
 
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = null; // para evitar problemas de plugin nao instalado no cordova
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, dbProvider: DatabaseProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();3
+      splashScreen.hide();
+      dbProvider.createDatabase()
+      .then(()=>{
+        this.abrirPaginaInicial(splashScreen)
+      })
+      .catch(()=>{
+        this.abrirPaginaInicial(splashScreen)
+      })
     });
+  }
+  private abrirPaginaInicial(splashScreen: SplashScreen)
+  {
+    splashScreen.hide;
+    this.rootPage=LoginPage;//agora sim chama a home page
+
   }
 }
 
